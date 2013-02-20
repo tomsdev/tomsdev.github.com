3ff471adfd0e23fb04e72361cb935fac1eff2236
tomsdev <74759@supinfo.com>
2013-02-19 19:49:06 -0800

    Add tags and filtering by tags

---
+++ src/helpers/tag_helper.js
@@ -0,0 +1,60 @@
+var helper_utils = require("punch").Utils.Helper;
+
+var block_helpers = {
+    /**
+     * Returns the given text parsed. It replaces the tags in the given text in the
+     * form @@tag and @@tag:text with an html link.
+     * @returns {String}
+     */
+    parse_tags: function() {
+        return helper_utils.checkArgs(arguments, function(text) {
+            if(!text) {
+                return "";
+            }
+            // replace format @@web:Website
+            var result = text.replace(/@@([a-z]*):([a-z-]*)/gi, "<a class='tag' href='#$1'>$2</a>");
+            // replace format @@web
+            return result.replace(/@@([a-z-]*)/gi, "<a class='tag' href='#$1'>$1</a>");
+        });
+    },
+
+    /**
+     * Returns a class attribute containing all the tags present in the given text.
+     * Note: the classes are in the form tag-web, tag-mobile, etc.
+     * @returns {String}
+     */
+    extract_tags_classes: function() {
+        return helper_utils.checkArgs(arguments, function(text) {
+            if(!text) {
+                return "";
+            }
+
+            var regExp = /@@([a-z-]*)/gi;
+            var tags = [];
+
+            while ((match = regExp.exec(text)) !== null) {
+                tags.push('tag-' + match[1]);
+            }
+
+            if (tags.length > 0) {
+                var classes = tags.join(' ');
+                return "class='" + classes + "'";
+            }
+            return "";
+        });
+    }
+};
+
+module.exports = {
+
+    directAccess: function(){
+        return { "block_helpers": block_helpers, "options": {} };
+    },
+
+    get: function(basepath, file_extension, options, callback){
+        var self = this;
+
+        return callback(null, { "block": block_helpers }, {});
+    }
+
+};
---
+++ src/templates/filter.js
@@ -0,0 +1,145 @@
+/**
+ * Executes the given function fn with each node that match the given selector.
+ * @param {String} selector
+ * @param {Function} fn
+ */
+function forEach(selector, fn) {
+    var nodeList = document.querySelectorAll(selector);
+    for (var i = 0, length = nodeList.length; i < length; i++) {
+        fn(nodeList[i]);
+    }
+}
+
+/**
+ * Changes the visibility for each node that match the given selector.
+ * @param {!String} selector
+ * @param {!Boolean} isVisible
+ * @param {?Function} beforeFn Optional function to execute on each node before
+ *                             changing its visibility.
+ */
+function SetNodesVisibility(selector, isVisible, beforeFn) {
+    var value = isVisible ? "block" : "none";
+    forEach(selector, function (node) {
+        if (beforeFn) {
+            beforeFn(node);
+        }
+        node.style.display = value;
+    });
+}
+
+/**
+ * Returns the selector corresponding to the given tag.
+ * @param {String} tag
+ * @returns {string}
+ */
+function getTagSelector(tag) {
+    return 'article.tag-' + tag;
+}
+
+/**
+ * Shows the clear filter button for the given tag.
+ * @param tag
+ */
+function showClearFilterBtn(tag) {
+    SetNodesVisibility(".clear-filter-btn", true, function (node) {
+        node.innerText = "stop filtering by tag " + tag;
+    });
+}
+
+/**
+ * Hides the clear filter button.
+ */
+function hideClearFilterBtn() {
+    SetNodesVisibility(".clear-filter-btn", false);
+}
+
+/**
+ * Caches the last filter used.
+ * @type {String}
+ */
+var lastFilter = null;
+
+/**
+ * Shows only the articles containing the given tag and hides the others.
+ * @param {String} tag
+ */
+function setFilterTag(tag) {
+    if (!tag) { return; }
+    var tagSelector = getTagSelector(tag);
+
+    if (lastFilter) {
+        // Hide articles with the lastFilter tag
+        SetNodesVisibility(lastFilter, false);
+        // Show articles with the given tag
+        SetNodesVisibility(tagSelector, true);
+    } else {
+        // Hide all articles without the given tag
+        SetNodesVisibility("article:not(.tag-" + tag + ")", false);
+    }
+    // cache the tag in lastFilter
+    lastFilter = tagSelector;
+    updateSectionsVisibility(tagSelector);
+    showClearFilterBtn(tag);
+}
+
+/**
+ * Extracts the tag from the href attribute of the given node and filters by this tag.
+ * @param node Markup of type <a>
+ */
+function filter(node) {
+    var tag = node.hash.substr(1);
+    setFilterTag(tag);
+}
+
+/**
+ * Stops filtering and shows all articles.
+ */
+function clearFilterTag() {
+    lastFilter = null;
+    hideClearFilterBtn();
+    // Show all articles
+    SetNodesVisibility("article", true);
+    // Show all sections
+    SetNodesVisibility("section", true);
+}
+
+/**
+ * For each section, hides it if it doesn't contain an article with the given tag,
+ * otherwise shows it.
+ * @param {String} tagSelector
+ */
+function updateSectionsVisibility(tagSelector) {
+    forEach('section', function (node) {
+        var articles = node.querySelectorAll(tagSelector);
+        if (articles.length > 0) {
+            node.style.display = 'block';
+        } else {
+            node.style.display = 'none';
+        }
+    });
+}
+
+/**
+ * Filters the articles in the page if there is a tag in the current url anchor.
+ * Otherwise shows all.
+ */
+function updateFilterWithCurrentHash() {
+    var hash = window.location.hash;
+    if (hash) {
+        var tag = hash.substr(1);
+        setFilterTag(tag);
+    } else {
+        clearFilterTag();
+    }
+    window.scrollTo(0, 0);
+}
+
+// Update the filter when the anchor in the url changes
+window.onhashchange = function () {
+    updateFilterWithCurrentHash();
+}
+
+// Update the filter when the page is loaded
+window.addEventListener('DOMContentLoaded', function() {
+    updateFilterWithCurrentHash();
+}, false);
